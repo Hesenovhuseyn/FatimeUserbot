a862645409f0f2205926df09ecc60637c7d231d3
HuseynHesenov <huseyn-hesenov-2022@mail.ru>
2025-11-20 21:04:08 +0400

    .

--- userbot/modules/__helpme.py
+++ userbot/modules/__helpme.py
@@ -16,7 +16,7 @@ async def yardim(event):
     if tgbotusername is not None:
         results = await event.client.inline_query(
             tgbotusername,
-            "@FatimeUb"
+            "@Botlar_Destek"
         )
         await results[0].click(
             event.chat_id,
